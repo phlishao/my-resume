94a13f06f0c50dba1b03cc59dcb74d93952a5176
phlishao <914797252@qq.com>
2019-04-15 20:39:17 +0800

    Delete SectionContent.js

--- src/components/Section/SectionContent.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-const SectionContent = props => {
-  const { children } = props;
-  return <ul className="sec-content">{children}</ul>;
-};
-
-export default SectionContent;
